8d65b54d3362c56401d908e1cd7d8a0d4e0dd0fb
David Brooks <david.brooks@talis.com>
2017-04-02 09:59:16 +0100

    Docblocks and tests for utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,13 +1,37 @@
-
+/**
+ * Is this an empty object?
+ *
+ * @param  {any} obj - a candidate object to check
+ * @return {boolean} - `true` if an empty Object; `false` otherwose
+ */
 export function isEmptyObject(obj: any): boolean {
   return (Object.keys(obj).length === 0) && (obj.constructor === Object)
 }
 
+/**
+ * Is this value defined?
+ *
+ * "Defined" here covers anything that is not "undefined".
+ *
+ * Falsy values may still defined if they have a valid type.
+ *
+ * @param  {any} value - a candidate value
+ * @return {boolean} - `true` if the value is defined (including null); `false` otherwise.
+ */
 export function isDefined(value: any): boolean {
   return typeof value !== 'undefined';
 }
 
-export function keyBy<T>(array: T[], keyFunc: (T) => string): { [key: string]: T } {
+type KeyingFunction<T> = (T) => string;
+
+/**
+ * Convert an array into a keyed object for constant-time lookup.
+ *
+ * @param {T[]} array - an array of objects to be keyed.
+ * @param {KeyingFunction<T>} keyFunc - a function converting array elements to keys
+ * @type {T} - the type of elements appareaing as values in the resultant object.
+ */
+export function keyBy<T>(array: T[], keyFunc: KeyingFunction<T>): { [key: string]: T } {
   const reducer = (soFar, element) => Object.assign(soFar, {
     [keyFunc(element)]: element,
   });
