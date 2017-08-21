0d026c7170f462ee869456b80a195dc7dd93a345
David Brooks <david.brooks@talis.com>
2017-08-21 22:51:05 +0100

    Adds sample data for multiple resources

---
+++ spec/serialisation/fake-multiple-response.json.ts
@@ -0,0 +1,72 @@
+export const FAKE_MULTIPLE_RESPONSE = {
+  "data": [
+    {
+      "id": "person1",
+      "type": "people",
+      "attributes": {
+        "firstName": "Eric",
+        "surname": "Wimp"
+      },
+      "relationships": {
+        "address": {
+          "data": {
+            "id": "address1",
+            "type": "addresses"
+          }
+        },
+        "old_addresses": {
+          "data": [
+            {
+              "id": "address2",
+              "type": "addresses"
+            }
+          ]
+        }
+      }
+    },
+    {
+      "id": "person2",
+      "type": "people",
+      "attributes": {
+        "firstName": "Bruce",
+        "surname": "Wayne"
+      },
+      "relationships": {
+        "address": {
+          "data": {
+            "id": "address2",
+            "type": "addresses"
+          }
+        }
+      }
+    }
+  ],
+  "included": [
+    {
+      "id": "address1",
+      "type": "addresses",
+      "attributes": {
+        "houseNumber": 29,
+        "street": "Acacia Road",
+        "city": "Nuttytown"
+      }
+    },
+    {
+      "id": "address2",
+      "type": "addresses",
+      "attributes": {
+        "houseNumber": 1007,
+        "street": "Mountain Drive",
+        "city": "Gotham City"
+      },
+      "relationships": {
+        "most_famous_inhabitant": {
+          "data": {
+            "id": "person2",
+            "type": "people"
+          }
+        }
+      }
+    }
+  ]
+}
