662d432105f87c278cd1e94c1db491d8c3506ef1
David Brooks <david.brooks@talis.com>
2018-11-06 15:38:43 +0000

    Export for build

--- lib/jsonapi/types.d.ts
+++ lib/jsonapi/types.d.ts
@@ -24,7 +24,6 @@ export interface PaginationLinksObject extends LinksObject {
     last?: string;
     next?: string;
     prev?: string;
-    self: string;
 }
 /**
  * JSON:API meta information objects
